e32b7d069021c819cb88785b44804e9b28f99993
Chris Tseng <samfire5200@gmail.com>
2022-06-17 16:09:16 +0800

    07-131. Other Auth Routes

---
+++ ticketing/auth/src/routes/signout.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+router.post('/api/users/signout', 
+(req, res) => {
+  res.send("Hi Signout!");
+});
+
+export { router as signoutRouter };
+
