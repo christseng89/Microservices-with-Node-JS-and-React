002ec32d2528a408e251a02c828b08a4f14e0bb6
Chris Tseng <samfire5200@gmail.com>
2022-06-18 10:35:03 +0800

    07-132. Adding Validation && 07-133. Handling Validation Errors

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,10 +1,32 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
-router.post('/api/users/signup', 
-(req, res) => {
-  res.send("Hi Signup!");
-});
+const minLen = 4;
+const maxLen = 20;
+
+router.post(
+  '/api/users/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage(`Password must be between ${minLen} and ${maxLen} characters`),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    const { email, password } = req.body;
+    // new User({ email, password })
+    res.status(200).send({email, password});
+  }
+);
 
 export { router as signupRouter };
 
