714e4c32a6a01eba2152528c0521f89c8f187218
Chris Tseng <samfire5200@gmail.com>
2022-06-20 10:23:30 +0800

    07-138. Building an Error Handling Middleware

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -9,6 +9,6 @@ export const errorHandler = (
   console.log("Something went wrong", err);
 
   res.status(400).send({
-    message: "Something went wrong: " + err.message,
+    error: err.message,
   });
 };
