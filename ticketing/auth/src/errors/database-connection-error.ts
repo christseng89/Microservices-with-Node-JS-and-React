0e2102151aae69daa38299cdd53fe76c58ffeb61
Chris Tseng <samfire5200@gmail.com>
2022-06-20 18:19:56 +0800

    07-144. Moving Logic Into Errors

--- ticketing/auth/src/errors/database-connection-error.ts
+++ ticketing/auth/src/errors/database-connection-error.ts
@@ -1,5 +1,6 @@
 export class DatabaseConnectionError extends Error {
   reason = "Error connecting to database";
+  statusCode = 500;
 
   constructor() {
     super();
@@ -7,4 +8,12 @@ export class DatabaseConnectionError extends Error {
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
+
+  serializeErrors() {
+    return [
+      {
+        message: this.reason,
+      },
+    ];
+  }  
 }
