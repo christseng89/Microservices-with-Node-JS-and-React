20a77a9b7e89e63a1033a5a6ad0e1f4e6744fb1a
Chris Tseng <samfire5200@gmail.com>
2022-06-21 13:00:56 +0800

    08-150. Connecting to MongoDB

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import { json } from "body-parser";
 import express from "express";
 import 'express-async-errors';
+import mongoose from 'mongoose';
 
 import { NotFoundError } from "./errors/not-found-error";
 import { currentUserRouter } from './routes/current-user';
@@ -25,6 +26,17 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!!!!!!!!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
+
+start();
