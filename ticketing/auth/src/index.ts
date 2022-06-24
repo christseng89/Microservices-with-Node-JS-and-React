bd8990bed952b62e3313d8461d39c2a83681bd98
Chris Tseng <samfire5200@gmail.com>
2022-06-24 11:07:06 +0800

    09-173. Adding Session Support

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -2,6 +2,7 @@ import { json } from "body-parser";
 import express from "express";
 import 'express-async-errors';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { NotFoundError } from "./errors/not-found-error";
 import { currentUserRouter } from './routes/current-user';
@@ -13,7 +14,12 @@ import { signupRouter } from './routes/signup';
 import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}));
 
 app.use(currentUserRouter);
 app.use(signupRouter);
