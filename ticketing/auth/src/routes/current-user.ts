2555c1927d042834450d0a084afda5be7364d2bb
Chris Tseng <samfire5200@gmail.com>
2022-06-28 01:47:30 +0800

    09-190. Requiring Auth for Route Access

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,17 +1,19 @@
 import express, { Request, Response} from 'express';
 
 import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 router.get('/api/users/currentuser', 
-currentUser,
-async (req: Request, res: Response) => {
-  if (!req.currentUser) {
-    return res.status(400).send({ currentUser: null });
-  }
+  currentUser,
+  requireAuth,
+  async (req: Request, res: Response) => {
+    if (!req.currentUser) {
+      return res.status(400).send({ currentUser: null });
+    }
 
-  res.status(200).send({ currentUser: req.currentUser });
-});
+    res.status(200).send({ currentUser: req.currentUser });
+  });
 
 export { router as currentUserRouter };
 //
