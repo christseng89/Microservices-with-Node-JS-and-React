9af01ff01a8c9245f00fddf60c2a45f6d4daaa42
Chris Tseng <samfire5200@gmail.com>
2022-06-27 18:00:48 +0800

    09-186. Returning the Current User

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,10 +1,22 @@
-import express from 'express';
+import express, { Request, Response} from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 router.get('/api/users/currentuser', 
-async (req, res) => {
-  res.send("Hi there from router!");
+async (req: Request, res: Response) => {
+  // 1. Check if there is a JWT in the session object
+  if (!req.session?.jwt) {
+    return res.status(400).send({ currentUser: null });
+  }
+
+  // 2. Verify the JWT
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.status(200).send({ currentUser: payload });
+  } catch (err) {
+    res.status(400).send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
-
+//
