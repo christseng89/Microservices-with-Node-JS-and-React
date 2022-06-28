e83dd3653dc9b73719a0fe3a21102cb8c20c2cc5
Chris Tseng <samfire5200@gmail.com>
2022-06-28 17:31:14 +0800

    10-203. Testing Sign Out

--- ticketing/auth/src/routes/signout.ts
+++ ticketing/auth/src/routes/signout.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 
 const router = express.Router();
-router.post('/api/users/signout', 
-async (req, res) => {
+router.post('/api/users/signout', async (req, res) => {
   // 1. Check if there is a JWT in the session object
   if (!req.session?.jwt) {
-    return res.status(400).send({ currentUser: null });
+    return res.status(200).send({ currentUser: null });
   }
 
   // 2. Remove the session object  
