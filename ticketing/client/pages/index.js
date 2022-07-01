c583d009762b88a1ae87b36f9b3c0b2e60781a8a
Chris Tseng <samfire5200@gmail.com>
2022-07-01 19:10:00 +0800

    11-226. Fetching Data During SSR

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,12 +1,24 @@
-const LandingPage = ({ color }) => {
-  console.log('I am in the component', color);
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log('I am in the component');
+  axios.get('/api/users/currentuser')
+  .catch(
+    err => {
+      // console.log(err.message)
+    }
+  );
+
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
-  console.log('I am on the server!');
-  return { color: 'red' };
-};
+// LandingPage.getInitialProps = async () => {
+//   console.log('I am on the server!');
+//   const response = await axios.get('/api/users/currentuser');
+
+//   return response.data;
+// };
 
 export default LandingPage;
+
 //#endregion
