34b2cf27978fbe553306796417c72c07d3a74c0c
Chris Tseng <samfire5200@gmail.com>
2022-07-02 22:08:59 +0800

    11-232. On the Server or the Browser

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,22 +1,21 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  // console.log('I am in the component');
-  // axios.get('/api/users/currentuser')
-  // .catch(
-  //   err => {
-  //     // console.log(err.message)
-  //   }
-  // );
+  // console.log('I am EXECUTED in the Browser!', currentUser);
+  // axios.get('/api/users/currentuser');
 
   return <h1>Landing Page</h1>;
 };
 
 LandingPage.getInitialProps = async () => {
-  console.log('I am EXECUTED!');
-  // const response = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser');
-
-  // return response.data;
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser
+    console.log('I am EXECUTED in the server!');
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
+  }
   return {};
 };
 
