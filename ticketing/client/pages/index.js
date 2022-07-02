bbc839fd20a3710160f4966d2eb9f5dfc09ae1ab
Chris Tseng <samfire5200@gmail.com>
2022-07-02 22:54:52 +0800

    11-234. Specifying the Host

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,24 +1,40 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  // console.log('I am EXECUTED in the Browser!', currentUser);
-  // axios.get('/api/users/currentuser');
-
-  return <h1>Landing Page</h1>;
+  console.log('Current user is: ', currentUser);
+  if (currentUser) {
+    return <h1>You are login.</h1>;
+  }
+  return <h1>You are NOT login.</h1>;
 };
 
 LandingPage.getInitialProps = async () => {
   if (typeof window === 'undefined') {
     // we are on the server!
     // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser
-    console.log('I am EXECUTED in the server!');
+    try {
+      const { data } = await axios.get(
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+        {
+          headers: {
+            Host: 'ticketing.com', // ingress-srv.yaml
+          },
+        }
+      );
+      return data;
+    } catch (error) {
+      return ({ currentUser: null });
+    }
   } else {
     // we are on the browser!
     // requests can be made with a base url of ''
+    try {
+      const { data } = await axios.get('/api/users/currentuser');
+      return data;
+    } catch (error) {
+      return ({ currentUser: null });
+    }
   }
-  return {};
 };
 
 export default LandingPage;
-
-//#endregion
