d1221c7772bca2eed71ae58078c85f1b8143bbd4
Chris Tseng <samfire5200@gmail.com>
2022-07-02 21:51:38 +0800

    11-230. Cross Namespace Service Communication

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,23 +1,24 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  console.log('I am in the component');
-  axios.get('/api/users/currentuser')
-  .catch(
-    err => {
-      // console.log(err.message)
-    }
-  );
+  // console.log('I am in the component');
+  // axios.get('/api/users/currentuser')
+  // .catch(
+  //   err => {
+  //     // console.log(err.message)
+  //   }
+  // );
 
   return <h1>Landing Page</h1>;
 };
 
-// LandingPage.getInitialProps = async () => {
-//   console.log('I am on the server!');
-//   const response = await axios.get('/api/users/currentuser');
+LandingPage.getInitialProps = async () => {
+  console.log('I am EXECUTED!');
+  // const response = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser');
 
-//   return response.data;
-// };
+  // return response.data;
+  return {};
+};
 
 export default LandingPage;
 
