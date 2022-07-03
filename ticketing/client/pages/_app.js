3fe6c4f8040f9176dbfda00b56a8b553a65b7bb1
Chris Tseng <samfire5200@gmail.com>
2022-07-03 18:20:59 +0800

    11-241. Issues with Custom App GetInitialProps

--- ticketing/client/pages/_app.js
+++ ticketing/client/pages/_app.js
@@ -10,8 +10,23 @@ const AppComponent = ({ Component, pageProps }) => {
   );
 };
 
-AppComponent.getInitialProps = async () => {
-  return {};
+AppComponent.getInitialProps = async (req) => {
+  const client = buildClient(req.ctx);
+  console.log(Object.keys(req)); // [ 'AppTree', 'Component', 'router', 'ctx' ]
+  try {
+    const { data } = await client.get('/api/users/currentuser');
+    console.log('Data in App.js :', data);
+    // [client] Data in App.js : {
+    // [client]   currentUser: {
+    // [client]     id: '62c1451c26b9d6b6556614fa',
+    // [client]     email: 'test@test.com',
+    // [client]     iat: 1656833308
+    // [client]   }
+    // [client] }
+    return data; 
+  } catch (error) {
+    return {};
+  }
 };
 
 export default AppComponent;
