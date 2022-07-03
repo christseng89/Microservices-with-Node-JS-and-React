fd1c0646b57718d5cb9fc2f317c56c887e912863
Chris Tseng <samfire5200@gmail.com>
2022-07-03 15:20:27 +0800

    11-237. Content on the Landing Page

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -2,10 +2,7 @@ import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log('Current user is: ', currentUser);
-  if (currentUser) {
-    return <h1>You are login.</h1>;
-  }
-  return <h1>You are NOT login.</h1>;
+  return currentUser ? <h1>You are signed in.</h1> : <h1>You are NOT signed in.</h1>;
 };
 
 LandingPage.getInitialProps = async (req) => {
