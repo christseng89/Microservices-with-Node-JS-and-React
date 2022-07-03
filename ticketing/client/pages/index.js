5d9bb563f490604f6f480e2063905a1fe8cb888c
Chris Tseng <samfire5200@gmail.com>
2022-07-03 15:11:02 +0800

    11-236. A Reusable API Client

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log('Current user is: ', currentUser);
@@ -8,30 +8,13 @@ const LandingPage = ({ currentUser }) => {
   return <h1>You are NOT login.</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // we are on the server!
-    // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser
-    try {
-      const { data } = await axios.get(
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-        {
-          headers: req.headers,
-        }
-      );
-      return data;
-    } catch (error) {
-      return ({ currentUser: null });
-    }
-  } else {
-    // we are on the browser!
-    // requests can be made with a base url of ''
-    try {
-      const { data } = await axios.get('/api/users/currentuser');
-      return data;
-    } catch (error) {
-      return ({ currentUser: null });
-    }
+LandingPage.getInitialProps = async (req) => {
+  const client = buildClient(req);
+  try {
+    const { data } = await client.get('/api/users/currentuser');
+    return data;   
+  } catch (error) {
+    return {};
   }
 };
 
