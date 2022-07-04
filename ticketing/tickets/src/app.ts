f0ceccba9466b7fef49b5d04b6000e932187e487
Chris Tseng <samfire5200@gmail.com>
2022-07-04 13:56:39 +0800

    12-259. Project Setup

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -4,11 +4,6 @@ import 'express-async-errors';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@cygnetops/common';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -16,11 +11,6 @@ app.use(cookieSession({
   signed: false,
   secure: process.env.NODE_ENV !== 'test'
 }));
-
-app.use(currentUserRouter);
-app.use(signupRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
