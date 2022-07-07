a25e24283df35645dcc850958be5c310f6c8b233
Chris Tseng <samfire5200@gmail.com>
2022-07-07 14:33:04 +0800

    13-278. Complete Index Route Implementation

--- ticketing/tickets/src/routes/__test__/index.test.ts
+++ ticketing/tickets/src/routes/__test__/index.test.ts
@@ -16,7 +16,10 @@ it('can fetch a list of tickets', async () => {
   await createTicket();
   await createTicket();
 
-  const response = await request(app).get('/api/tickets').send().expect(200);
+  const response = await request(app)
+    .get('/api/tickets')
+    .send()
+    .expect(200);
 
   expect(response.body.length).toEqual(3);
 });
