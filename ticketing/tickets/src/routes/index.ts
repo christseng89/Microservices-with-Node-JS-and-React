cc7e7374fdfc82ec6ce2fe3557a4808f539c2674
Chris Tseng <samfire5200@gmail.com>
2022-07-07 14:35:21 +0800

    13-278. Complete Index Route Implementation

--- ticketing/tickets/src/routes/index.ts
+++ ticketing/tickets/src/routes/index.ts
@@ -4,10 +4,10 @@ import { Ticket } from '../models/ticket';
 const router = express.Router();
 
 router.get('/api/tickets', 
-  async (req: Request, res: Response) => {
+  async (_req: Request, res: Response) => {
     const tickets = await Ticket.find({});
 
-    res.send(tickets);
+    res.status(200).send(tickets);
 });
 
 export { router as indexTicketRouter };
