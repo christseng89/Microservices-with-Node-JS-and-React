c7e5ff3f8578e17b7c7a218447ff7238005bae41
Chris Tseng <samfire5200@gmail.com>
2022-07-08 14:44:00 +0800

    14-296. Publishing Events

--- ticketing/nats-test/src/publisher.ts
+++ ticketing/nats-test/src/publisher.ts
@@ -1,9 +1,19 @@
 import nats from 'node-nats-streaming';
 
-const stan = nats.connect('ticketing', 'abc', { // stan = client
+const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Publisher connected to NATS');
+
+  const data = JSON.stringify({
+    id: '123',
+    title: 'Concert',
+    price: 23,
+  });
+
+  stan.publish('ticket:created', data, () => {
+    console.log('Event published');
+  });
 });
