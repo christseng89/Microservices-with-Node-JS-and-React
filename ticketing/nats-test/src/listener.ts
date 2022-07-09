3f4ca56d6c55e6f41918d1686db6f80b2c8f5f9e
Chris Tseng <samfire5200@gmail.com>
2022-07-09 12:12:24 +0800

    14-304. Manual Ack Mode

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -2,16 +2,29 @@ import nats, { Message } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 
 console.clear();
-const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
-  url: 'http://localhost:4222',
-});
+const stan = nats.connect(
+  'ticketing', 
+  randomBytes(4).toString('hex'), {
+    url: 'http://localhost:4222',
+  }
+);
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
+  const options = stan.subscriptionOptions()
+    .setManualAckMode(true);
+  const subscription = stan.subscribe(
+    'ticket:created', 
+    'orders-listener-queue-group',
+    options
+  );
 
-  const subscription = stan.subscribe('ticket:created');
+  subscription.on(
+    'message', 
+    (msg: Message) => {
+      console.log(`Received event: #${msg.getSequence()}, with data ${msg.getData()}`);
 
-  subscription.on('message', (msg: Message) => {
-    console.log(`Received event: #${msg.getSequence()}, with data ${msg.getData()}`);
-  });
+      msg.ack();
+    }
+  );
 });
