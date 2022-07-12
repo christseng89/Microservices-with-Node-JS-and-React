dc47fb3a00d76c62c445ba20ced75097d240d7ca
Chris Tseng <samfire5200@gmail.com>
2022-07-12 13:23:35 +0800

    16-332. More on Publishing ~ 337. Accessing the NATS Client

--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -4,6 +4,7 @@ import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@chinasystems/common';
 import { Ticket } from '../models/ticket';
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 router.post(
@@ -25,7 +26,7 @@ router.post(
     });
 
     await ticket.save();
-    await new TicketCreatedPublisher(client)
+    await new TicketCreatedPublisher(natsWrapper.client)
       .publish({
         id: ticket.id,
         title: ticket.title,
