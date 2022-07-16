62ae5a3844513341d4be5cbba21daf63be6eff04
Chris Tseng <samfire5200@gmail.com>
2022-07-16 19:47:18 +0800

    18-386. Testing Event Publishing

--- ticketing/orders/src/routes/__test__/new.test.ts
+++ ticketing/orders/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import request from 'supertest';
 import { Order, OrderStatus } from '../../models/order';
 import { Ticket } from '../../models/ticket';
 import { app } from '../../app';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('returns an error if the ticket does not exist', async () => {
   const ticketId = new mongoose.Types.ObjectId();
@@ -56,4 +57,20 @@ it('reserves a ticket', async () => {
     .expect(201);
 });
 
-it.todo('publishes an order created event');
+it('publishes an order created event', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.fakeSignup())
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
