ac79262b51f14e1f6301800ca4c5a5822bf90a5b
Chris Tseng <samfire5200@gmail.com>
2022-07-21 20:28:07 +0800

    21-462. Another Order Model!

---
+++ ticketing/payments/src/models/order.ts
@@ -0,0 +1,70 @@
+import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
+import { OrderStatus } from '@chinasystems/common';
+
+export { OrderStatus };
+// An interface that describes the properties
+// that are required to create a new Order
+interface OrderAttrs {
+  id: string; // order.id
+  version: number; // order.version
+  userId: string;
+  status: OrderStatus;
+  price: number;
+}
+
+// An interface that describes the properties
+// that a Order Document has
+interface OrderDoc extends mongoose.Document {
+  version: number;  
+  userId: string;
+  status: OrderStatus;
+  price: number;
+}
+
+// An interface that describes the properties
+// that a Order Model has
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(_doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
+orderSchema.pre('save', async function(done) {
+  done();
+});
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+export { Order };
