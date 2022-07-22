5cd689c325079999cc7daffae21764c6425d4b2b
Chris Tseng <samfire5200@gmail.com>
2022-07-22 15:35:20 +0800

    21-471. Validating Order Payment ~ 472. Testing Order Validation Before Payment

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -1,27 +1,33 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { requireAuth, validateRequest, BadRequestError, NotFoundError, } from '@chinasystems/common';
+import { requireAuth, validateRequest, BadRequestError, NotFoundError, NotAuthorizedError, OrderStatus,} from '@chinasystems/common';
 
 import { Order } from '../models/order';
 
 const router = express.Router();
+router.post(
+  '/api/payments',
+  requireAuth, [
+    body('token').not().isEmpty(), 
+    body('orderId').not().isEmpty()
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+    const order = await Order.findById(orderId);
 
-try {
-  router.post(
-    '/api/payments',
-    requireAuth, [
-      body('token').not().isEmpty(), 
-      body('orderId').not().isEmpty()
-    ],
-    validateRequest,
-    async (_req: Request, res: Response) => {
-      res.send({ success: true });
+    if (!order) {
+      throw new NotFoundError();
     }
-  );
-} catch (err) {
-  console.log(err);
-  throw new BadRequestError('Invalid request parameters');
-}
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for an cancelled order');
+    }
 
+    res.send({ success: true });
+  }
+);
 
 export { router as createChargeRouter };
