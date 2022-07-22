8fbe61fbd89d0124731cdcdfd735ab8b5b54adae
Chris Tseng <samfire5200@gmail.com>
2022-07-22 12:04:54 +0800

    21-466. Marking an Order as Cancelled ~ 468. Starting the Listeners

--- ticketing/payments/src/index.ts
+++ ticketing/payments/src/index.ts
@@ -1,9 +1,10 @@
 
 import mongoose from 'mongoose';
+
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
-// import { OrderCreatedListener } from './events/listeners/order-created-listener';
-// import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -38,8 +39,8 @@ const start = async () => {
     process.on('SIGTERM', () => stan.close());
   
     // Start listening for orders events
-    // new OrderCreatedListener(natsWrapper.client).listen();
-    // new OrderCancelledListener(natsWrapper.client).listen();
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
     
     await mongoose.connect(process.env.MONGO_URI, {});
     console.log('Connected to MongoDb');
