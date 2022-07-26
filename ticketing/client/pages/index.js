b622fcd48cd1d6a8dddc7de9d921a135749d586d
Chris Tseng <samfire5200@gmail.com>
2022-07-26 15:26:48 +0800

    Bug fix for current-user.ts

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,9 +1,12 @@
 const LandingPage = ({ currentUser }) => {
-  console.log('Current user is: ', currentUser);
-  return currentUser ? <h1>You are signed in.</h1> : <h1>You are NOT signed in.</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
-LandingPage.getInitialProps = async (_ctx, _client, _currentUser) => {
+LandingPage.getInitialProps = async (context, client, currentUser) => {
   return {};
 };
 
