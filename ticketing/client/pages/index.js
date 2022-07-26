eb030bfcb21d742b1f672ff9639d1c0d9078c8c3
Chris Tseng <samfire5200@gmail.com>
2022-07-27 02:29:02 +0800

    22-496. Listing All Tickets

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,10 +1,39 @@
-const LandingPage = ({ currentUser }) => {
-  // console.log('Current user is: ', currentUser);
-  return currentUser ? <h1>You are signed in.</h1> : <h1>You are NOT signed in.</h1>;
+const LandingPage = ({ currentUser, tickets }) => {
+  console.log('Tickets', tickets);
+  if (!tickets) {
+    return currentUser ? 
+      <h1>You are signed in.</h1> : 
+      <h1>You are NOT signed in.</h1>;
+  }
+
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
+  );
 };
 
-LandingPage.getInitialProps = async (_ctx, _client, _currentUser) => {
-  return {};
+LandingPage.getInitialProps = async (_context, client, _currentUser) => {
+  const { data } = await client.get('/api/tickets');
+  return { tickets: data };
 };
 
 export default LandingPage;
