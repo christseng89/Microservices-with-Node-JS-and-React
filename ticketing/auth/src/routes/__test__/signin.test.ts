79eab842a546692dad1d9951e4c5f44d447db204
Chris Tseng <samfire5200@gmail.com>
2022-07-28 14:48:31 +0800

    23-515. Output of Failing Tests #2

--- ticketing/auth/src/routes/__test__/signin.test.ts
+++ ticketing/auth/src/routes/__test__/signin.test.ts
@@ -30,7 +30,7 @@ it('responds with a cookie when given valid credentials', async () => {
       email: 'test@test.com',
       password: 'password'
     })
-    .expect(400);
+    .expect(200);
 
   expect(response.get('Set-Cookie')).toBeDefined();
 });
